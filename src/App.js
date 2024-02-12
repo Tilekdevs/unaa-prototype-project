8ab93af3df4aa6c9a2f4fd4b5df1f333e2e170e1
tilekF <gchncbv@gmail.com>
2024-02-12 16:25:27 +0600

    update package.json

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Layout from './layout/Layout';
-import AboutUs from './pages/AboutUs/AboutUs';
-import Camera from './pages/Camera/Camera';
-import Home from './pages/Home/Home';
-import Jobs from './pages/Jobs/Jobs';
-import News from './pages/News/News';
-import NewsDetails from './pages/News/NewsDetails/NewsDetails';
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Layout from './layout/Layout'
+import AboutUs from './pages/AboutUs/AboutUs'
+import Camera from './pages/Camera/Camera'
+import Home from './pages/Home/Home'
+import Jobs from './pages/Jobs/Jobs'
+import News from './pages/News/News'
+import NewsDetails from './pages/News/NewsDetails/NewsDetails'
 import CalculatorServices from './pages/Services/Calculator/CalculatorServices'
-import NotFound from './pages/NotFound/NotFound';
+import RequestInspection from './pages/Services/RequestForInspection/RequestInspection.jsx'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
-  return (
-    <div className='App'>
-      <Routes>
-        <Route path={'/'} element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path={'/about'} element={<AboutUs />} />
-          <Route path={'/news'} element={<News />} />
-          <Route path={'/news/:id'} element={<NewsDetails />} />
-          <Route path={'/calculator'} element={<CalculatorServices />} />
-          <Route path={'/camera'} element={<Camera />} />
-          <Route path={'/jobs'} element={<Jobs />} />
-          <Route path={'*'} element={<NotFound />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Routes>
+				<Route path={'/'} element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path={'/about'} element={<AboutUs />} />
+					<Route path={'/news'} element={<News />} />
+					<Route path={'/news/:id'} element={<NewsDetails />} />
+					<Route path={'/calculator'} element={<CalculatorServices />} />
+					<Route
+						path={'/request-for-inspection'}
+						element={<RequestInspection />}
+					/>
+					<Route path={'/camera'} element={<Camera />} />
+					<Route path={'/jobs'} element={<Jobs />} />
+					<Route path={'*'} element={<NotFound />} />
+				</Route>
+			</Routes>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,139 +1,139 @@
 // BurgerMenu.js
 
-import React, { useState } from 'react';
-import { slide as Menu } from 'react-burger-menu';
-import { IoMdClose } from 'react-icons/io';
-import { HiMenuAlt3 } from 'react-icons/hi';
-import { Link } from 'react-router-dom';
-import RegisterForm from '../RegisterForm/RegisterForm';
-import './burgerMenu.scss';
-import { FaAngleDown } from 'react-icons/fa';
+import React, { useState } from 'react'
+import { slide as Menu } from 'react-burger-menu'
+import { IoMdClose } from 'react-icons/io'
+import { HiMenuAlt3 } from 'react-icons/hi'
+import { Link } from 'react-router-dom'
+import RegisterForm from '../RegisterForm/RegisterForm'
+import './burgerMenu.scss'
+import { FaAngleDown } from 'react-icons/fa'
 
 const BurgerMenu = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const [isModalOpen, setModalOpen] = useState(false);
+	const [isMenuOpen, setMenuOpen] = useState(false)
+	const [isDropdownOpen, setDropdownOpen] = useState(false)
+	const [isModalOpen, setModalOpen] = useState(false)
 
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-  };
+	const toggleMenu = () => {
+		setMenuOpen(!isMenuOpen)
+	}
 
-  const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen);
-  };
+	const toggleDropdown = () => {
+		setDropdownOpen(!isDropdownOpen)
+	}
 
-  const toggleModal = () => {
-    setModalOpen(!isModalOpen);
-    closeMenu();
-  };
+	const toggleModal = () => {
+		setModalOpen(!isModalOpen)
+		closeMenu()
+	}
 
-  const closeMenu = () => {
-    setMenuOpen(false);
-  };
+	const closeMenu = () => {
+		setMenuOpen(false)
+	}
 
-  return (
-    <div className='burger-menu'>
-      <div className='burger-icon' onClick={toggleMenu}>
-        <HiMenuAlt3 />
-      </div>
+	return (
+		<div className='burger-menu'>
+			<div className='burger-icon' onClick={toggleMenu}>
+				<HiMenuAlt3 />
+			</div>
 
-      {isMenuOpen && <div className='overlay' onClick={closeMenu}></div>}
+			{isMenuOpen && <div className='overlay' onClick={closeMenu}></div>}
 
-      <Menu
-        isOpen={isMenuOpen}
-        onStateChange={(state) => {
-          if (state.isOpen !== isMenuOpen) {
-            setMenuOpen(state.isOpen);
-          }
-        }}
-        customBurgerIcon={false}
-        customCrossIcon={false}
-        width={'300px'}
-        right
-        styles={{
-          bmBurgerButton: {
-            display: 'none',
-          },
-          bmCrossButton: {
-            height: '24px',
-            width: '24px',
-          },
-          bmCross: {
-            background: '#bdc3c7',
-          },
-          bmMenu: {
-            background: '#1e88e5',
-            padding: '2.5em 1.5em 0',
-            fontSize: '1.15em',
-          },
-          bmMenuWrap: {
-            top: '0px',
-            zIndex: '6000',
-            right: '-1px',
-          },
-          bmMorphShape: {
-            fill: '#373a47',
-          },
-          bmItemList: {
-            color: '#ffffff',
-            padding: '0.8em',
-          },
-          bmOverlay: {
-            background: 'rgba(0, 0, 0, 0.3)',
-          },
-        }}
-      >
-        <div className='close-icon' onClick={closeMenu}>
-          <IoMdClose />
-        </div>
-        <ul className='menu-items'>
-          <li onClick={toggleModal}>
-            <Link to='/'>Войти/Регистрация</Link>
-          </li>
-          <li onClick={closeMenu}>
-            <Link to='/'>Главная</Link>
-          </li>
-          <li onClick={closeMenu}>
-            <Link to='/about'>О нас</Link>
-          </li>
-          <li onClick={closeMenu}>
-            <Link to='/news'>Новости</Link>
-          </li>
-          <li
-            onClick={(e) => {
-              e.stopPropagation();
-              toggleDropdown();
-            }}
-            className={isDropdownOpen ? 'open' : ''}
-          >
-            <p className='burger__menu-title'>
-              Сервисы <FaAngleDown />
-            </p>
-            {isDropdownOpen && (
-              <ul className='dropdown-menu'>
-                <li>
-                  <Link to='/calculator'>Калькулятор</Link>
-                </li>
-                <li>
-                  <Link to='/form'>Форма обращения</Link>
-                </li>
-              </ul>
-            )}
-          </li>
-          <li onClick={closeMenu}>
-            <Link to='/contacts'>Информация</Link>
-          </li>
-          <li onClick={closeMenu}>
-            <Link to='/jobs'>Вакансии</Link>
-          </li>
-          <li onClick={closeMenu}>
-            <Link to='/camera'>Онлайн Камеры</Link>
-          </li>
-        </ul>
-      </Menu>
-      {isModalOpen && <RegisterForm onClose={toggleModal} />}
-    </div>
-  );
-};
+			<Menu
+				isOpen={isMenuOpen}
+				onStateChange={state => {
+					if (state.isOpen !== isMenuOpen) {
+						setMenuOpen(state.isOpen)
+					}
+				}}
+				customBurgerIcon={false}
+				customCrossIcon={false}
+				width={'300px'}
+				right
+				styles={{
+					bmBurgerButton: {
+						display: 'none',
+					},
+					bmCrossButton: {
+						height: '24px',
+						width: '24px',
+					},
+					bmCross: {
+						background: '#bdc3c7',
+					},
+					bmMenu: {
+						background: '#1e88e5',
+						padding: '2.5em 1.5em 0',
+						fontSize: '1.15em',
+					},
+					bmMenuWrap: {
+						top: '0px',
+						zIndex: '6000',
+						right: '-1px',
+					},
+					bmMorphShape: {
+						fill: '#373a47',
+					},
+					bmItemList: {
+						color: '#ffffff',
+						padding: '0.8em',
+					},
+					bmOverlay: {
+						background: 'rgba(0, 0, 0, 0.3)',
+					},
+				}}
+			>
+				<div className='close-icon' onClick={closeMenu}>
+					<IoMdClose />
+				</div>
+				<ul className='menu-items'>
+					<li onClick={toggleModal}>
+						<Link to='/'>Войти/Регистрация</Link>
+					</li>
+					<li onClick={closeMenu}>
+						<Link to='/'>Главная</Link>
+					</li>
+					<li onClick={closeMenu}>
+						<Link to='/about'>О нас</Link>
+					</li>
+					<li onClick={closeMenu}>
+						<Link to='/news'>Новости</Link>
+					</li>
+					<li
+						onClick={e => {
+							e.stopPropagation()
+							toggleDropdown()
+						}}
+						className={isDropdownOpen ? 'open' : ''}
+					>
+						<p className='burger__menu-title'>
+							Сервисы <FaAngleDown />
+						</p>
+						{isDropdownOpen && (
+							<ul className='dropdown-menu'>
+								<li>
+									<Link to='/calculator'>Калькулятор</Link>
+								</li>
+								<li>
+									<Link to='/request-for-inspection'>Форма обращения</Link>
+								</li>
+							</ul>
+						)}
+					</li>
+					<li onClick={closeMenu}>
+						<Link to='/contacts'>Информация</Link>
+					</li>
+					<li onClick={closeMenu}>
+						<Link to='/jobs'>Вакансии</Link>
+					</li>
+					<li onClick={closeMenu}>
+						<Link to='/camera'>Онлайн Камеры</Link>
+					</li>
+				</ul>
+			</Menu>
+			{isModalOpen && <RegisterForm onClose={toggleModal} />}
+		</div>
+	)
+}
 
-export default BurgerMenu;
+export default BurgerMenu
