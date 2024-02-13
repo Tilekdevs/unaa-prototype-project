909534d43ec40d067f245d75e24b340d7e886b3c
AlisherFronT <alisher.rysspaevjava@gmail.com>
2024-02-13 17:02:50 +0600

    upd:search

--- src/layout/Header/HeaderTop/HeaderTop.jsx
+++ src/layout/Header/HeaderTop/HeaderTop.jsx
@@ -1,65 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import Fuse from 'fuse.js'; 
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Fuse from "fuse.js";
+import axios from "axios";
+import CustomModal from "../../../components/CustomModal/CustomModal";
+import { FaSearch } from "react-icons/fa";
+import whatsapp from "../../../assets/img/whatsapp.png";
+import facebook from "../../../assets/img/facebook.png";
 
 const HeaderTop = () => {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
   const [data, setData] = useState([]);
-  const [showResults, setShowResults] = useState(false); // Состояние для отображения результатов
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/news')
-      .then(response => {
+    axios
+      .get("http://127.0.0.1:8000/api/news")
+      .then((response) => {
         setData(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching data:', error);
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
   }, []);
 
   useEffect(() => {
     const fuseOptions = {
-      keys: ['title', 'text', 'published_date'],
+      keys: ["title", "text", "published_date"],
       includeScore: true,
       threshold: 0.4,
     };
-    
+
     const fuse = new Fuse(data, fuseOptions);
-    
+
     const searchResults = fuse.search(query);
-    const filteredResults = searchResults.map(result => result.item);
+    const filteredResults = searchResults.map((result) => result.item);
     setResults(filteredResults);
-  }, [query, data]);  
+  }, [query, data]);
 
   const handleSearch = () => {
-    setShowResults(true); // Показываем результаты поиска при нажатии на кнопку
+    setShowModal(true);
   };
 
   const handleQueryChange = (e) => {
-    setQuery(e.target.value); // Обновляем значение запроса
-    setShowResults(false); // Сбрасываем состояние showResults, чтобы скрыть результаты
+    setQuery(e.target.value);
+    setShowModal(false);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
   };
 
   return (
-    <div>
-      <input 
-        value={query}
-        onChange={handleQueryChange}
-      />
-      <button onClick={handleSearch}>Search</button> 
-      
-      {showResults && ( 
-        <ul>
-          {results && results.length > 0 ? (
-            results.map((item, index) => (
-              <li key={index}>{item.title}</li>
-            ))
-          ) : (
-            <li>No results found</li>
-          )}
-        </ul>
-      )}
+    <div className="header__top">
+      <div className="header__top-icons">
+        <img className="header__top-social" src={facebook} alt="" />
+        <img className="header__top-social" src={whatsapp} alt="" />
+      </div>
+
+      <div className="header__top-search">
+        <input
+          value={query}
+          onChange={handleQueryChange}
+          className="header__top-input"
+        />
+        <FaSearch onClick={handleSearch} className="header__top-icon" />
+
+        <CustomModal
+          open={showModal}
+          handleClose={handleCloseModal}
+          results={results}
+        />
+      </div>
     </div>
   );
 };
