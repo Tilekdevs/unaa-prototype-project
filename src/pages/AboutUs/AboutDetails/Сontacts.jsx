a71bff71487ce7bbadd170962b6cd88f4f5a623d
alisherfront <alisher.rysspaevjava@gmail.com>
2024-03-06 09:28:53 +0600

    upd:contactPage

--- src/pages/AboutUs/AboutDetails/Сontacts.jsx
+++ src/pages/AboutUs/AboutDetails/Сontacts.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import '../aboutUs.scss'
+import React from "react";
+import "../aboutUs.scss";
+import { MdWorkOutline } from "react-icons/md";
 
 const Contacts = ({ data }) => {
-	return (
-		<div className='MainCardItem'>
-			<div key={data.id} className='card'>
-				<div className='info'>
-					<div className='title'>
-						<h2 className='MainTitle'>{` ${data.title || 'Н/Д'}`}</h2>
-						<p className='address'>{`Адрес: ${data.address || 'Н/Д'}`}</p>
-						<p className='phone'>{`Тел:  ${data.phone || 'Н/Д'}`}</p>
-						<p className='time_job'>{`График работы: ${
-							data.time_job || 'Н/Д'
-						}`}</p>
-						<br />
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <>
+      <div className="search__top">
+        <div className="search__select">
+          <select name="" id="" className="jobs__top-select">
+            <option value="" className="jobs__top-option">
+              Бишкек
+            </option>
+          </select>
 
-export default Contacts
+          <select name="" id="" className="jobs__top-select">
+            <option value="" className="jobs__top-option">
+              Бишкек
+            </option>
+          </select>
+
+          <select name="" id="" className="jobs__top-select">
+            <option value="" className="jobs__top-option">
+              Бишкек
+            </option>
+          </select>
+        </div>
+
+        <input type="text" placeholder="Поиск" className="search__input" />
+      </div>
+      <div className="MainCardItem">
+        <div key={data.id} className="card">
+          <div className="info">
+            <MdWorkOutline className="icon" />
+            <div className="title">
+              <p className="place">г.Бишкек</p>
+              <p className="MainTitle">{` ${data.title || "Н/Д"}`}</p>
+              <p>{`${data.phone || "Н/Д"}`}</p>
+            </div>
+            <div className="address">
+              <p>{`${data.address || "Н/Д"}`}</p>
+            </div>
+            <p className="date">{`${data.time_job || "Н/Д"}`}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Contacts;
--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,89 +1,85 @@
-import Box from '@mui/material/Box'
-import Tab from '@mui/material/Tab'
-import Tabs from '@mui/material/Tabs'
-import Typography from '@mui/material/Typography'
-import PropTypes from 'prop-types'
-import * as React from 'react'
-import ContactList from '../../utils/_ContactsData'
-import History from './AboutDetails/History'
-import Managment from './AboutDetails/Managment/Managment'
-import Contacts from './AboutDetails/Сontacts'
-import './aboutUs.scss'
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import Tabs from '@mui/material/Tabs';
+import Typography from '@mui/material/Typography';
+import PropTypes from 'prop-types';
+import * as React from 'react';
+import ContactList from '../../utils/_ContactsData';
+import History from './AboutDetails/History';
+import Managment from './AboutDetails/Managment/Managment';
+import Contacts from './AboutDetails/Сontacts';
+import './aboutUs.scss';
 
 function CustomTabPanel(props) {
-	const { children, value, index, ...other } = props
+    const { children, value, index, ...other } = props;
 
-	return (
-		<div
-			role='tabpanel'
-			hidden={value !== index}
-			id={`simple-tabpanel-${index}`}
-			aria-labelledby={`simple-tab-${index}`}
-			{...other}
-		>
-			{value === index && (
-				<Box sx={{ p: 3 }}>
-					<Typography>{children}</Typography>
-				</Box>
-			)}
-		</div>
-	)
+    return (
+        <div
+            role='tabpanel'
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
 }
 
 CustomTabPanel.propTypes = {
-	children: PropTypes.node,
-	index: PropTypes.number.isRequired,
-	value: PropTypes.number.isRequired,
-}
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
 
 function a11yProps(index) {
-	return {
-		id: `simple-tab-${index}`,
-		'aria-controls': `simple-tabpanel-${index}`,
-	}
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
 }
 
 export default function AboutUs() {
-	const [value, setValue] = React.useState(0)
+    const [value, setValue] = React.useState(0);
 
-	const handleChange = (event, newValue) => {
-		setValue(newValue)
-	}
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
-	return (
-		<div className='MainTabsContainer'>
-			<div className='container'>
-				<Box sx={{ width: '100%' }}>
-					<Box
-						sx={{
-							borderBottom: 1,
-							borderColor: 'divider',
-							display: 'flex',
-							position: 'relative',
-							justifyContent: 'center',
-						}}
-					>
-						<Tabs
-							value={value}
-							onChange={handleChange}
-							aria-label='basic tabs example'
-						>
-							<Tab label='Руководство' {...a11yProps(0)} />
-							<Tab label='История' {...a11yProps(1)} />
-							<Tab label='Контакты' {...a11yProps(2)} />
-						</Tabs>
-					</Box>
-					<CustomTabPanel value={value} index={0}>
-						<Managment />
-					</CustomTabPanel>
-					<CustomTabPanel value={value} index={1}>
-						<History />
-					</CustomTabPanel>
-					<CustomTabPanel value={value} index={2}>
-						<ContactList contacts={Contacts} />
-					</CustomTabPanel>
-				</Box>
-			</div>
-		</div>
-	)
+    return (
+        <div className='MainTabsContainer'>
+            <div className='container'>
+                <Box sx={{ width: '100%' }}>
+                    <Box
+                        sx={{
+                            borderBottom: 1,
+                            borderColor: 'divider',
+                            display: 'flex',
+                            position: 'relative',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <Tabs
+                            value={value}
+                            onChange={handleChange}
+                            aria-label='basic tabs example'
+                        >
+                            <Tab label='Руководство' {...a11yProps(0)} />
+                            <Tab label='Контакты' {...a11yProps(1)} /> 
+                        </Tabs>
+                    </Box>
+                    <CustomTabPanel value={value} index={0}>
+                        <Managment />
+                    </CustomTabPanel>
+                    <CustomTabPanel value={value} index={1}>
+                        <ContactList contacts={Contacts} />
+                    </CustomTabPanel>
+                </Box>
+            </div>
+        </div>
+    );
 }
